63bd9c69b8f684166b03434cb290cad76fb65de3
mucharobert23 <mucharobert23@gmail.com>
2019-12-26 20:56:09 +0100

    Refactoring

--- HostsEditor/Areas/MainWindow/Models/UrlListItem.cs
+++ HostsEditor/Areas/MainWindow/Models/UrlListItem.cs
@@ -1,13 +1,10 @@
 ﻿using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HostsEditor.Areas.MainWindow.Models
 {
     public class UrlListItem : BindableBase
     {
-        private bool isSelected=false;
+        private bool isSelected = false;
         private string url;
         private bool isFromHost;
 
@@ -18,14 +15,14 @@ namespace HostsEditor.Areas.MainWindow.Models
             IsFromHost = false;
         }
 
-        public UrlListItem(string url,bool isSelected, bool isFromHost)
+        public UrlListItem(string url, bool isSelected, bool isFromHost)
         {
             Url = url;
             IsSelected = isSelected;
             IsFromHost = isFromHost;
         }
 
-        public string Url { get => url; set =>SetProperty(ref url,value); }
+        public string Url { get => url; set => SetProperty(ref url, value); }
         public bool IsSelected { get => isSelected; set => SetProperty(ref isSelected, value); }
         public bool IsFromHost { get => isFromHost; private set => SetProperty(ref isFromHost, value); }
     }
